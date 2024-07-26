63dc9820339667ba323d644fe1eaf85732a3cd11
tutorial29 <martha.vo.r.nd.r.a.n@gmail.com>
2024-07-26 16:22:07 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -4,12 +4,12 @@ const fs = require("fs")
 
 //aumto presence update
 global.AntiCallBot = true // false a i dah chuan, rawn call-tu che automatic a block a ni lovang
-global.GroupWcMsg = true // false a i dah chuan welcome message i off ang
+global.GroupWcMsg = false // false a i dah chuan welcome message i off ang
 global.autoTyping = false // true a i dah chuan i type la a lang ang
 global.autoRecord = false // true a i dah chuan Voice record angin i lang ang
 global.available = true // false a i dah chuan offline angin a awm ang
 global.public = true // false ah i dah chuan nang chauhin i hmang thei ang
-global.autoread = false // true a i dah chuan message zawng zawng read vek zelin a awm ang 
+global.autoread = true // true a i dah chuan message zawng zawng read vek zelin a awm ang 
 global.autoread_status = false // true a i dah chuan auto status view in a awm ang
 global.default_language = true // default language i hman dawn loh chuan false ah hia  dah tur
 global.mtl_language = false // multi language i hman dawn chuan true ah dah tur
